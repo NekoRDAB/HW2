8cf1bd0ba8194cdf468a857a7791f0b04f917992
Danil <ferrum268@mail.ru>
2023-11-19 19:51:44 +0500

    finish

--- main.py
+++ main.py
@@ -9,27 +9,16 @@ PORT = 65432
 
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            data = conn.recv(1024).decode().replace("\n", "").split()
-            url = data[-1]
-            dns_server = DNSServer(url)
-            ip = dns_server.find_ip_address()
-            conn.sendall(ip.encode())
-
-    # parser = argparse.ArgumentParser(description="dns server")
-    # parser.add_argument(
-    #     "url", type=str,
-    #     help="URL you wish to get ip address of"
-    # )
-    # args = parser.parse_args()
-    # url = args.url
-    # dns_server = DNSServer(url)
-    # ip = dns_server.find_ip_address()
-    # print(f"IP address for domain {url}: {ip}")
+    parser = argparse.ArgumentParser(description="dns server")
+    parser.add_argument(
+        "url", type=str,
+        help="URL you wish to get ip address of"
+    )
+    args = parser.parse_args()
+    url = args.url
+    dns_server = DNSServer(url)
+    ip = dns_server.find_ip_address()
+    print(f"IP address for domain {url}: {ip}")
 
 
 if __name__ == "__main__":
